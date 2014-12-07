8c4b9f50e9ed96d96f102efa1970cf42b059bd20
Kevin Carter <kevin.carter@rackspace.com>
2014-12-07 03:45:18 -0600

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@
 #
 # (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
 
-set -o -v -e
+set -e
 
 GIT_REPO="https://github.com/ansible/ansible"
 GIT_RELEASE="${GIT_RELEASE:-v1.8.2}"
@@ -66,3 +66,46 @@ fi
 
 # Install ansible
 pip2 install "${WORKING_DIR}" || pip install "${WORKING_DIR}"
+
+# Create rpc ansible wrapper tool
+cat > /usr/local/bin/rpc-ansible <<EOF
+#!/usr/bin/env bash
+# Copyright 2014, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
+
+# RPC wrapper tool to ease the use of ansible with multiple variable files.
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+function info() {
+    echo -e "\e[0;35m\${@}\e[0m"
+}
+
+# Discover the variable files.
+VAR1="\$(for i in \$(ls /etc/rpc_deploy/user_*.yml); do echo -ne "-e @\$i "; done)"
+
+# Provide information on the discovered variables.
+info "Variable files: \"\${VAR1}\""
+
+# Run the ansible playbook command.
+\$(which ansible-playbook) \${VAR1} \$@
+EOF
+
+# Ensure wrapper tool is executable
+chmod +x /usr/local/bin/rpc-ansible
+
+echo "rpc-ansible script created."
+echo "System is bootstrapped and ready for use."
